989b5984d66bc81ac8b8b55d4caae291e56835d6
Sunnywpnk <sunnywpnk@gmail.com>
2024-10-17 00:06:50 +0700

    login

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,31 +1,194 @@
-<!-- src/views/Home.vue -->
 <template>
-    <div>
-      <h1>Authen</h1>
-      <button @click="goTosentOTPPage">Go forgot pass</button>
-      <button @click="goToRegisterPage">register</button>
-      <button @click="goToMyVocabPage">Login</button>
+  <div class="container-login">
+  <div class="wrapper">
+    <div class="title-text">
+      <div v-if="isLogin" class="title login">Login Form</div>
+      <div v-else class="title signup">Signup Form</div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  
-  const goToMyVocabPage = () => {
-    router.push({ name: 'MyVocab' });
-  };
+    <div class="form-container">
+      <div class="form-inner">
+        <!-- Login Form -->
+        <form v-if="isLogin" @submit.prevent="handleLogin">
+          <div class="field">
+            <input type="text" v-model="loginForm.email" placeholder="Email Address" required />
+          </div>
+          <div class="field">
+            <input type="password" v-model="loginForm.password" placeholder="Password" required />
+          </div>
+          <div class="pass-link">
+            <a href="#">Forgot Password?</a>
+          </div>
+          <div class="field">
+            <input type="submit" value="Login" />
+          </div>
+          <div class="signup-link">
+            Not a member? <a href="#" @click.prevent="toggleForm">Create Account</a>
+          </div>
+        </form>
 
-  const goTosentOTPPage = () => {
-    router.push({ name: 'sentOTP' });
-  };
+        <!-- Signup Form -->
+        <form v-else @submit.prevent="handleSignup">
+          <div class="field">
+            <input type="text" v-model="signupForm.email" placeholder="Email Address" required />
+          </div>
+          <div class="field">
+            <input type="password" v-model="signupForm.password" placeholder="Password" required />
+          </div>
+          <div class="field">
+            <input type="submit" value="Sign Up" />
+          </div>
+          <div class="signup-link">
+            Already a member? <a href="#" @click.prevent="toggleForm">Login</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
 
-  const goToRegisterPage = () => {
-    router.push({ name: 'Register' });
-  };
-  </script>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
   
-  <style scoped>
+const router = useRouter();
 
-  </style>
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+interface SignupForm {
+  email: string;
+  password: string;
+}
+
+export default defineComponent({
+  name: 'LoginSignup',
+  setup() {
+    const isLogin = ref(true);
+
+    const loginForm = ref<LoginForm>({
+      email: '',
+      password: ''
+    });
+
+    const signupForm = ref<SignupForm>({
+      email: '',
+      password: ''
+    });
+
+    const toggleForm = () => {
+      isLogin.value = !isLogin.value;
+    };
+
+    const handleLogin = () => {
+      router.push({ name: 'MyVocab' });
+
+      console.log('Logging in with:', loginForm.value);
+    };
+
+    const handleSignup = () => {
+      console.log('Signing up with:', signupForm.value);
+    };
+
+    return {
+      isLogin,
+      loginForm,
+      signupForm,
+      toggleForm,
+      handleLogin,
+      handleSignup
+    };
+  }
+});
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
+
+.container-login{
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: -webkit-linear-gradient(left, #3131A3, #fa4299);
+}
+
+.wrapper {
+  width: 400px;
+  background: #fff;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.1);
+}
+
+.wrapper .title-text {
+  display: flex;
+  width: 200%;
+}
+
+.wrapper .title-text .title {
+  width: 50%;
+  font-size: 35px;
+  font-weight: 600;
+  text-align: center;
+}
+
+.wrapper .form-container {
+  width: 100%;
+}
+
+.form-inner form .field {
+  height: 50px;
+  width: 100%;
+  margin-top: 20px;
+}
+
+.form-inner form .field input {
+  height: 100%;
+  width: 100%;
+  outline: none;
+  padding-left: 15px;
+  font-size: 17px;
+  border-radius: 5px;
+  border: 1px solid lightgrey;
+  border-bottom-width: 2px;
+  transition: all 0.4s ease;
+}
+
+.form-inner form .field input:focus {
+  border-color: #fc83bb;
+}
+
+.form-inner form .pass-link {
+  margin-top: 5px;
+}
+
+.form-inner form .pass-link a,
+.form-inner form .signup-link a {
+  color: #3131A3;
+  text-decoration: none;
+}
+
+.form-inner form .signup-link {
+  text-align: center;
+  margin-top: 30px;
+}
+
+.form-inner form .pass-link a:hover,
+.form-inner form .signup-link a:hover {
+  text-decoration: underline;
+}
+
+form .field input[type="submit"] {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 500;
+  padding-left: 0px;
+  border: none;
+  cursor: pointer;
+  background: -webkit-linear-gradient(left, #3131A3, #fa4299);
+}
+</style>
